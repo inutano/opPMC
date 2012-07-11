71e9462102d124632211b2a5ddc2f545726f4995
tazro inutano ohta <inutano@gmail.com>
2012-07-11 17:57:58 +0900

    create class and fix to it

--- idconverter_pmc.rb
+++ idconverter_pmc.rb
@@ -5,6 +5,40 @@ require "open-uri"
 
 require "ap"
 
+module PubMedIDConverter
+  PMC_ids = ""
+  PMC_file_list = ""
+  
+  def self.load_table(pmc_ids_path, pmc_file_list_path)
+    PMC_ids <<  pmc_ids_path
+    PMC_file_list << pmc_file_list_path
+  end
+  
+  class PubMed
+    def initialize(pmid)
+      @pmid = pmid
+    end
+    
+    def pmcid
+      grepped = `grep #{@pmid} #{PMC_ids}`
+      grepped.split(",")[8]
+    end
+  
+    def pmc_xml
+      pmcid = self.pmcid
+      if pmcid
+        grepped = `grep #{pmcid} #{PMC_file_list}`
+        parsed = grepped.split("\t")[1]
+        if parsed
+          journal_name = parsed.split(/\s\d{4}\s/).first.gsub(".","").gsub(" ","_")
+          subbed = parsed.gsub(".","").gsub(";","").gsub(":","_").gsub(" ","_")
+          journal_name + "/" + subbed + ".nxml"
+        end
+      end
+    end
+  end
+end
+
 def get_sra_publication
   url = "http://sra.dbcls.jp/cgi-bin/publication2.php"
   publications = open(url).read
@@ -12,31 +46,27 @@ def get_sra_publication
   json_raw[ :ResultSet ][ :Result ]
 end
 
-def get_sra_pmc(pmc_ids_path, sra_publication)
-  sra_pmc = sra_publication.map do |entry|
-    sra_id = entry[:sra_id]
-    pmid = entry[:pmid]
-    grepped = `grep #{pmid} #{pmc_ids_path}`
-    pmc_id = grepped.split(",")[8]
-    pmc_id if pmc_id
-  end
-  sra_pmc.delete_if{|id| id == nil }.uniq
-end
-
 if __FILE__ == $0
   pmc_ids_path = "./PMC-ids.csv"
+  pmc_file_list_path = "./file_list.txt"
+  PubMedIDConverter.load_table(pmc_ids_path, pmc_file_list_path)
+
   sra_publication = get_sra_publication
-  sra_pmc_id_json = "./sra_pmc_id.json"
-  unless File.exist?(sra_pmc_id_json)
-    sra_pmc_id = get_sra_pmc(pmc_ids_path, sra_publication)
-    open(sra_pmc_id_json,"w"){|f| JSON.dump(sra_pmc_id, f) }
-  else
-    sra_pmc_id = open(sra_pmc_id_json){|f| JSON.load(f) }
+  pmc_xml_list = sra_publication.map do |entry|
+    pmid = entry[:pmid]
+    pm = PubMedIDConverter::PubMed.new(pmid)
+    pmcid = pm.pmcid
+    ap pmcid
+    pm.pmc_xml if pmcid
   end
   
-  table = "./file_list.txt"
-  sra_pmc_id[0..9].each do |id|
-    ap id
-    ap `grep #{id} #{table}`
+  purified = pmc_xml_list.select{|n| n }.uniq
+  open("pmc_xml_list.json","w"){|f| JSON.dump(purified, f)}
+  purified.each do |xml|
+    base = "./pmc_xml/"
+    fpath = base + xml
+    ap fpath
+    ap File.exist?(fpath)
   end
+  ap purified.length
 end
